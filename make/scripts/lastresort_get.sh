d4cd9b93c8c0058965fb44c9de6c7718fef8f781
nullgemm <nullgemm@mailbox.org>
2021-11-05 22:48:54 +0100

    add lastresort font license to pack

--- make/scripts/lastresort_get.sh
+++ make/scripts/lastresort_get.sh
@@ -12,3 +12,8 @@ cd res/noto/files || exit
 
 curl -L "https://github.com/unicode-org/last-resort-font/releases/download/$version/LastResortHE-Regular.ttf" \
 	-o LastResortHE-Regular.ttf
+
+cd ../licenses
+
+curl -L "https://raw.githubusercontent.com/unicode-org/last-resort-font/main/LICENSE.md" \
+	-o lastresort_license.txt
