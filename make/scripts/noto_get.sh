685b38ee9ef64e329c16f5312475c5dce478e83b
nullgemm <nullgemm@mailbox.org>
2022-01-18 13:41:26 +0100

    remove fonts with a UI variant

--- make/scripts/noto_get.sh
+++ make/scripts/noto_get.sh
@@ -18,7 +18,29 @@ mv ./LICENSE_OFL.txt ../licenses/noto_license.txt
 
 cd ..
 rm -rf tmp
+
+# remove Noto Color Emoji
 rm files/NotoEmoji*
+
+# remove unneeded font styles
 rm files/NotoMono*
 rm files/NotoSerif*
 rm files/NotoSansMono*
+
+# remove fonts with a UI variant
+rm files/NotoSansDevanagari-Regular.ttf
+rm files/NotoSansSinhala-Regular.ttf
+rm files/NotoNaskhArabic-Regular.ttf
+rm files/NotoSansMalayalam-Regular.ttf
+rm files/NotoSansGujarati-Regular.ttf
+rm files/NotoSansMyanmar-Regular.ttf
+rm files/NotoSansArabic-Regular.ttf
+rm files/NotoSansGurmukhi-Regular.ttf
+rm files/NotoSansTamil-Regular.ttf
+rm files/NotoSansBengali-Regular.ttf
+rm files/NotoSansTelugu-Regular.ttf
+rm files/NotoSansOriya-Regular.ttf
+rm files/NotoSansKannada-Regular.ttf
+rm files/NotoSansThai-Regular.ttf
+rm files/NotoSansKhmer-Regular.ttf
+rm files/NotoSansLao-Regular.ttf
